3885307547d6090ffb9675f19a6b0c8fd2be95f9
vivishow <78064059@qq.com>
2019-06-25 10:35:33 +0800

    add module and api

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const koa = require("koa");
+const bodyParser = require("koa-bodyparser");
 const app = new koa();
 const router = require("./route");
+
+app.use(bodyParser());
 
 // app.use(async ctx => {
 //   ctx.body = "hello world";
@@ -8,4 +11,6 @@ const router = require("./route");
 
 app.use(router.routes());
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("http://127.0.0.1:3000");
+});
--- route.js
+++ route.js
@@ -1,14 +1,51 @@
+require("dotenv").config();
 const Router = require("koa-router");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectId;
+const URI = process.env.DB;
+const dbname = "vcproject";
 
 const router = new Router();
 
 router.get("/", async (ctx, next) => {
-  ctx.body = `<h1>Index</h1>`;
+  ctx.body = `<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>`;
 });
 
 router.get("/hello/:name", async (ctx, next) => {
   const name = ctx.params.name;
   ctx.body = `<h1>Hello, ${name}!</h1>`;
+});
+
+router.get("/api/members", async (ctx, next) => {
+  const client = new MongoClient(URI, { useNewUrlParser: true });
+  try {
+    await client.connect();
+    const col = client.db(dbname).collection("vivi");
+    let r = await col.find({}).toArray();
+  } catch (error) {
+    console.log(error);
+    ctx.body = "error";
+  }
+  client.close();
+});
+
+router.post("/api/members", async (ctx, next) => {
+  const client = new MongoClient(URI, { useNewUrlParser: true });
+  try {
+    await client.connect();
+    const col = client.db(dbname).collection("vivi");
+    let r = await col.find({}).toArray();
+  } catch (error) {
+    console.log(error);
+    ctx.body = "error";
+  }
+  client.close();
+});
+
+router.post("/api/test", async (ctx, next) => {
+  console.log(ctx.request.body);
+
+  ctx.body = ctx.request.body;
 });
 
 module.exports = router;
