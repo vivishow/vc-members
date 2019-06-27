4844cd90a4c5445a91b61cca8b53b0172a794885
vivishow <78064059@qq.com>
2019-06-27 22:29:19 +0800

    update apis route

--- route.js
+++ route.js
@@ -18,34 +18,96 @@ router.get("/hello/:name", async (ctx, next) => {
 
 router.get("/api/members", async (ctx, next) => {
   const client = new MongoClient(URI, { useNewUrlParser: true });
-  try {
-    await client.connect();
-    const col = client.db(dbname).collection("vivi");
-    let r = await col.find({}).toArray();
-  } catch (error) {
-    console.log(error);
-    ctx.body = "error";
-  }
+  await client.connect();
+  const col = client.db(dbname).collection("vivi");
+  let r = await col.find({}).toArray();
+  ctx.body = r;
+  client.close();
+});
+
+router.get("/api/members/:id", async (ctx, next) => {
+  const id = ObjectId(ctx.params.id);
+  const client = new MongoClient(URI, { useNewUrlParser: true });
+  await client.connect();
+  const col = client.db(dbname).collection("vivi");
+  let r = await col.findOne({ _id: id });
+  ctx.body = r;
   client.close();
 });
 
 router.post("/api/members", async (ctx, next) => {
   const client = new MongoClient(URI, { useNewUrlParser: true });
-  try {
+  const data = ctx.request.body;
+  if (data.nickName) {
+    const time = new Date();
     await client.connect();
     const col = client.db(dbname).collection("vivi");
-    let r = await col.find({}).toArray();
-  } catch (error) {
-    console.log(error);
-    ctx.body = "error";
+    let r = await col.insertOne({
+      nickName: data.nickName,
+      noteName: data.noteName || "",
+      contactAddr: data.contactAddr || "",
+      contactPhone: data.contactPhone || "",
+      contact: data.contact || [],
+      points: 0,
+      pointsRecord: [],
+      create_on: time,
+      update_on: time
+    });
+    ctx.body = r.ok ? "success" : "error";
+    client.close();
+  } else {
+    ctx.body = "nickName is required";
   }
-  client.close();
 });
+
+router.post("/api/members/:id/points", async (ctx, next) => {
+  const data = ctx.request.body;
+  if (data.reason && Number(data.value)) {
+    const client = new MongoClient(URI, { useNewUrlParser: true });
+    await client.connect();
+    const col = client.db(dbname).collection("vivi");
+    const id = ObjectId(ctx.params.id);
+    const time = new Date();
+    const pointsRecord = {
+      id: ObjectId(),
+      createtime: time,
+      reason: data.reason,
+      value: Number(data.value)
+    };
+    let r = await col.findOneAndUpdate(
+      { _id: id },
+      {
+        $push: { pointsRecord: pointsRecord },
+        $inc: { points: pointsRecord.value },
+        $set: { update_on: time }
+      },
+      {
+        returnOriginal: false
+      }
+    );
+    ctx.body = r.ok ? "success" : "error";
+    client.close();
+  } else {
+    ctx.body = "reason and value is required";
+  }
+});
+
+router.post("/api/members/:id/contact", async (ctx, next) => {});
+
+router.post("/api/members/:id", async (ctx, next) => {});
+
+router.delete("/api/members/:id", async (ctx, next) => {});
+
+router.delete("/api/members/:id/contact", async (ctx, next) => {});
 
 router.post("/api/test", async (ctx, next) => {
   console.log(ctx.request.body);
 
   ctx.body = ctx.request.body;
+});
+
+router.get("/api/test/:any", async (ctx, next) => {
+  ctx.body = ctx.params;
 });
 
 module.exports = router;
