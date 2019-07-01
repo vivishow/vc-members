36735aeed764d0d02a3e7b6d5ab655d5ba851b30
vivishow <78064059@qq.com>
2019-07-02 01:16:18 +0800

    update port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const koa = require("koa");
 const bodyParser = require("koa-bodyparser");
 const app = new koa();
 const router = require("./route");
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser());
 
@@ -16,6 +17,6 @@ app.use(async (ctx, next) => {
 
 app.use(router.routes());
 
-app.listen(3000, () => {
-  console.log("http://127.0.0.1:3000");
+app.listen(PORT, () => {
+  console.log(`http://127.0.0.1:${PORT}`);
 });
