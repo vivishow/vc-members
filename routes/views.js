f8820199dea0c80fa52d9c7c080e34ba60c2e67a
vivishow <78064059@qq.com>
2019-07-20 15:38:36 +0800

    rewrite html with vue

--- routes/views.js
+++ routes/views.js
@@ -4,29 +4,9 @@ const axios = require("axios");
 const router = new Router();
 
 router.get("/", async (ctx, next) => {
-  let res = await axios.get(`http://${ctx.host}/api/members`);
-  await ctx.render("index", { users: res.data.message });
-});
-
-router.get("/member-info/:id", async (ctx, next) => {
-  let id = ctx.params.id;
-  let host = ctx.request.host;
-  let res = await axios.get(
-    `https://vc-members.herokuapp.com/api/members/${id}`
-  );
-  if (res.data.code == -1) {
-    ctx.redirect("/");
-  } else {
-    await ctx.render("member-info", {
-      host: host,
-      ...res.data.message
-    });
-  }
-  //   ctx.body = res.data.data;
-});
-
-router.get("/test", async (ctx, next) => {
-  await ctx.render("test");
+  // let res = await axios.get(`http://${ctx.host}/api/members`);
+  // await ctx.render("index", { users: res.data.message });
+  await ctx.render("index");
 });
 
 module.exports = router;
